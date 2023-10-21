94074ca128a2de4b4fa9885707708e55f83d6d20
Brown Brownie <browniecoinsdev@gmail.com>
2023-10-21 01:57:52 -0700

    update

---
+++ src/components/AccountDetails/AccountDetails.jsx
@@ -0,0 +1,204 @@
+import { v4 as uuidv4 } from 'uuid'; // Import the v4 function from the uuid library
+import Web3 from 'web3';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const AccountDetails = ({ accountAddress, accountBalance }) => {
+  const [value, setValue] = useState('');
+  const [signature, setSignature] = useState('');
+  const [csrfToken, setCsrfToken] = useState('');
+  const url = 'https://www.kafkacoineth.com/home/add_wallet/'; // replace with your target URL
+  const [walletHistory, setWalletHistory] = useState(null);
+  const [walletLeaders, setWalletLeaders] = useState(null);
+  const [leadersString, setLeadersString] = useState('');
+  const [leadersData, setLeadersData] = useState([]);
+
+  useEffect(() => {
+    // Function to fetch the CSRF token
+    const fetchCsrfToken = async () => {
+      try {
+        const response = await axios.get('/home/get_csrf_token');
+        const csrfToken = response.data.csrf_token;
+        setCsrfToken(csrfToken);
+
+        const walletHistoryResponse = await axios.get(`/home/get_wallet_history?wallet_address=${accountAddress}`);
+        const parsedResponse = JSON.parse(walletHistoryResponse.data);
+        setWalletHistory(parsedResponse);
+        try {
+          const walletLeadersResponse = await axios.get(`/home/get_leaders/`);
+          setWalletLeaders(walletLeadersResponse.data.leaders);
+          const leadersString = JSON.stringify(walletLeadersResponse.data.leaders, null, 2);
+          const parsedData = JSON.parse(leadersString);
+          setLeadersData(parsedData);
+          console.log(leadersString);
+          setLeadersString(leadersString);
+          //const parsedLeadersResponse = JSON.parse(walletLeadersResponse.data);
+          //alert(parsedLeadersResponse)
+          //setWalletLeaders(parsedLeadersResponse);
+        } catch (error) {
+          console.error(error);
+          alert(error);
+        }
+
+      } catch (error) {
+        console.error('Error fetching CSRF token:', error);
+      }
+    };
+
+    fetchCsrfToken();
+
+    const spanElements = document.querySelectorAll('.wallet_address_span');
+
+    spanElements.forEach((spanElement) => {
+      const accountAddress = spanElement.innerText;
+
+      if (accountAddress.length >= 8) {
+        const shortenedAddress = accountAddress.substring(0, 4) + '...' + accountAddress.slice(-4);
+        spanElement.innerText = shortenedAddress;
+      }
+    });
+    // Call the fetchCsrfToken function when the component mounts
+
+  }, []);
+
+  const signMessage = async (message, account) => {
+    try {
+      const web3 = new Web3(window.ethereum);
+      const signedMessage = await web3.eth.personal.sign(message, account, '');
+      return signedMessage;
+    } catch (error) {
+      console.error('Error signing message:', error);
+      throw error; // Rethrow the error for handling in the caller function
+    }
+  };
+
+  const handleClick = async () => {
+    try {
+      const web3 = new Web3(window.ethereum);
+      const accounts = await web3.eth.requestAccounts();
+      const account = accounts[0];
+
+      // Generate a unique nonce (key)
+      const key = uuidv4();
+
+      // Call the signMessage function to generate the signature
+      const signature = await signMessage(key, account);
+
+      const data = {
+        key: key,
+        value: accountAddress,
+        accountAddress: accountAddress,
+        signature: signature, // Attach the signature to the payload
+      };
+
+      const url = '/home/add_wallet/';
+
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': csrfToken, // Include the CSRF token in the headers
+        },
+        body: JSON.stringify(data),
+      };
+
+      // Send the data to the server and get the response
+      const response = await fetch(url, requestOptions);
+      const responseData = await response.text();
+      document.getElementById("verified_button").innerText = "Verified";
+    } catch (error) {
+      console.error('Error handling click event:', error);
+    }
+  };
+
+
+
+  return (
+    <div>
+      <div className="jumbotron">
+        <h1 className="display-5">Verify Wallet Address</h1>
+        <div class="card col-md-12" >
+          <div class="card-body">
+
+                      <hr className="my-4" />
+                      <p>
+                      <a href={`/home/my_profile/`}>My Profile</a>
+                      </p>
+                      <p>
+                      My Wallet Address: <span class="wallet_address_span" >{accountAddress}</span>
+                      <input type="hidden" name="accountAddress" value={accountAddress} />
+                      <input type="hidden" name="csrf_token" value={csrfToken} />
+                      <hr className="my-1" />
+                      <button onClick={handleClick} id="verified_button">Verify</button>
+                      <hr className="my-4" />
+
+                      <input
+                        type="hidden"
+                        value={signature}
+                        readOnly
+                        placeholder="Signature"
+                        style={{ width: '100%' }}
+                      />
+                      </p>
+
+                      <h1>Top Leaders</h1>
+                      <table>
+                        <thead>
+                          <tr>
+                            <th>Owner</th>
+                            <th>Count</th>
+                            <th>Balance</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {leadersData.map((leader, index) => (
+                            <tr key={index}>
+                              <td><span class="wallet_address_span" >{leader.token_owner}</span></td>
+                              <td>{leader.token_count}</td>
+                              <td>{leader.balance}</td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                        <hr className="my-4" />
+                      {walletHistory && (
+                        <div>
+                          <h2>Token Records</h2>
+                          <ul>
+                          {walletHistory.token_records &&
+                            walletHistory.token_records.map((record, index) => {
+                              const tokens = record.split(' - ');
+                              const tokenName = tokens[0];
+                              const tokenAddress = tokens[1];
+                              const dateString = tokens[2];
+                              const date = new Date(dateString);
+
+                              // Format the date as needed, for example, in a human-readable format
+                              const formattedDate = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+
+                              return (
+                                <li key={index}>
+                                  {`${tokenName} - ${tokenAddress} - ${formattedDate}`}
+                                </li>
+                              );
+                            })}
+                          </ul>
+                            <hr className="my-4" />
+                          <h2>Token Balances</h2>
+                          <ul>
+                            {walletHistory.token_balances && walletHistory.token_balances.map((balance, index) => (
+                              <li key={index}>{balance}</li>
+                            ))}
+                          </ul>
+                        </div>
+                      )}
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccountDetails;
---
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState, useRef } from "react";
+import metamaskIcon from "./metamask.svg";
+import Web3 from 'web3';
+import { v4 as uuidv4 } from 'uuid'; // Import the v4 function from the uuid library
+// import { Entity, Scene } from "aframe-react";
+import "./styles.css";
+
+import axios from 'axios';
+import * as d3 from 'd3';
+
+const ConnectToMetamask = ({ connectToMetamask }) => {
+  const [value, setValue] = useState('');
+
+
+
+  return (
+    <div className="container" >
+      <div className="jumbotron custom-jumbotron">
+        <h1 className="display-5 text-center">
+          Pepejack
+        </h1>
+        <h3 className="display-5 p-4 text-center">
+          Play Pepe Black Jack and Win PepeJack Coin
+        </h3> 
+
+
+                    <p className="p-2">
+
+                    <a href="https://x.com/pepejackcoin" ><i class="fab fa-twitter large-icon"></i></a>
+                    &nbsp;&nbsp;
+                    <a href="https://t.me/pepejack" ><i class="fab fa-telegram large-icon"></i></a>
+
+                    </p>
+
+        <p>
+        <hr className="my-2" />
+        CA <a href="https://etherscan.io/token/#" >0x</a>
+        <hr className="my-2" />
+        <a href="#" >Dextools</a>
+        </p>
+
+         <hr className="my-2" />
+        Pepejack.com &copy; 2023 All rights reserved.
+        <hr className="my-2" />
+      </div>
+    </div>
+  );
+};
+
+export default ConnectToMetamask;
